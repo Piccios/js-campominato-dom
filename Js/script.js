e8a9feeefecb90ba3f29c2ff50e79a41bec26761
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-20 17:48:24 +0200

    corrections in function "generateNewGame"; created a new function to generate bombs in random cells

--- Js/script.js
+++ Js/script.js
@@ -5,41 +5,47 @@ const gridElement = document.querySelector('section#grid_100');
 const playButton = document.querySelector('button#play');
 
 
-playButton.addEventListener('click', function(){
+playButton.addEventListener('click', function () {
     generateNewGame(100, gridElement)
 })
 
-function generateNewGame(numberOfCells, containerElement){
+function generateNewGame(numberOfCells, containerElement,bombs) {
     containerElement.innerHTML = '';
-for (let index = 0; index < numberOfCells; index++) {
-    const squareElement = document.createElement('article'); 
-    squareElement.classList.add('square');
-    squareElement.addEventListener('click', function(){
+    // Creo una lista di numeri casuali da 1 a "numero di celle"
+    for (let index = 0; index < numberOfCells; index++) {
+        const squareElement = document.createElement('article');
+        squareElement.classList.add('square');
+    // Event listener per il click su ogni quadrato
+    squareElement.addEventListener('click', function () {
+        if (bombs.includes(index + 1)) {
+        squareElement.classList.add('bomb');
+        alert('Hai cliccato una bomba! Hai perso!');
+        } else {
         squareElement.classList.add('active');
-        
-        console.log((index) + 1);
+        }
+},  { once: true });
 
-    }, {once : true})
+        squareElement.append((index) + 1);
+        gridElement.appendChild(squareElement);
+        const refreshButton = document.querySelector('button#play');
 
-    squareElement.append((index) + 1);
-    gridElement.appendChild(squareElement);
-    const refreshButton = document.querySelector('button#play');
-
-refreshButton.addEventListener('click', function(){squareElement.classList.remove('active');
-}) 
-}}
-
-
-// Creo un array di 16 numeri casuali, non ripetibili, da 1 a 100 che inseriro' nella funzione della griglia
-
-let bombs = [];
-
-for (let index = 0; index < 16; index++ ){
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    if (bombs.indexOf(randomNumber) === -1){
-        bombs.push(randomNumber);
+        refreshButton.addEventListener('click', function () {
+            squareElement.classList.remove('active');
+        })
     }
-    bombs
+}
+
+
+// Funzione per generare le bombe
+function generateBombs(numberOfCells) {
+    let bombs = [];
+    while (bombs.length < 16) {
+        let randomNumber = Math.floor(Math.random() * numberOfCells) + 1;
+        if (!bombs.includes(randomNumber)) {
+            bombs.push(randomNumber);
+        }
+    }
+    return bombs;
 }
 
 
